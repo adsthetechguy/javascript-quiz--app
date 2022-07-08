42977bcc4caa515eefa9f8889a4431eac1a299f3
solomon abu <104644315+adsthetechguy@users.noreply.github.com>
2022-07-08 16:21:27 +0100

    Add files via upload

---
+++ quiz app/questions.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+  number:1,        
+  question:'What is the Capital of Lagos?', 
+  answer:'Ikeja', 
+  options:[
+          'Mushin', 
+          'Agege', 
+          'Ikeja', 
+          'Ojota'
+          ]
+    }, 
+    {
+            number:2,
+            question: 'Which is the only body part that is fully grown from birth?',
+            answer: 'Eyes',
+            options: [
+          'Hands',
+          'legs',
+          'Nose',
+          'Eyes'
+          ]
+    }, 
+    {
+            number:3,
+            question: 'What year year did Nigeria got her independence?',
+            answer: 'October 1960',
+            options: [
+          'October 1960',
+          'August 1995',
+          'May 1960',
+          'None of the above'
+          ]
+    }, 
+    {
+            number:4,
+            question: 'What does FIFA stand for in English??',
+            answer: 'International Federation of Association Football',
+            options: [
+          'International Fellowship of Association Football',
+          'International Federal of Association Football',
+          'International Federation of Association Football',
+          'None of the above'
+          ]
+    }, 
+    {
+            number:5,
+            question:'What is the capital of Ireland? ',
+            answer:'Dublin',
+            options:[
+          'United Kingdom',
+          'Dublin',
+          'Norway',
+          'Navada'
+          ]
+    }, 
+ ]
